08abb9ae2ed278e4d89d9afafebe2fb990e9db86
Alex McRitchie <amcritchie@gmail.com>
2014-08-13 09:37:45 -0600

    prints the 3 functions and there answers.

--- methods.rb
+++ methods.rb
@@ -1,9 +1,9 @@
 def say_hi
-  print 'Hi!'
+  p 'Hi!'
 end
 
 def add(a,b)
-  print a + b
+  p a + b
 end
 
 def range
@@ -15,8 +15,17 @@ end
 
 def in_range(number,range)
   if number >= range[:start] && number <= range[:end]
-    print 'yup'
+    p 'yup'
   else
-    print 'nope!'
+    p 'nope!'
   end
 end
+
+print 'say_hi => '
+say_hi
+
+print 'add(3,8) => '
+add(3,8)
+
+print "in_range(42,range) => "
+in_range(42,range)
